32359cd74b0fa757bee98f39989ca6a1e165d321
shadowslayer111 <26972048+shadowslayer111@users.noreply.github.com>
2025-10-31 15:35:47 -0500

    Fixed enemy rotation in the middle of the board

--- Game.py
+++ Game.py
@@ -11,13 +11,14 @@ def print_gameboard(gameboard): #prints the gameboard when the gameboard is pass
             print(gameboard[row,collom],end="  ")
         print()
 game_board = create_board(5) #hardcodes the gameboard size, maybe make it dynamic later
-user_guess = input("Enter a guess to make: ")
-user_row_guess = int(user_guess[0]) #Take the first part of the user guess to parse later
-user_collum_guess = int(user_guess[-1]) #Take the second part of the user guess
-game_board[user_row_guess, user_collum_guess] = "x"
+#user_guess = input("Enter a guess to make: ")
+#user_row_guess = int(user_guess[0]) #Take the first part of the user guess to parse later
+#user_collum_guess = int(user_guess[-1]) #Take the second part of the user guess
+#game_board[user_row_guess, user_collum_guess] = "x"
 #Very jankey way to do exention with 0 being up,1 being right,2 being down and 3 being left
 inital_enemy_placement_row = random.randint(1,5)
 inital_enemy_placement_collum = random.randint(1,5)
+enemy_rotation = random.randint(0,3)
 #For if inital placement is on the edge to prevent going past the playable area
 if inital_enemy_placement_row == 1:
     enemy_rotation = random.randint(1,3)
@@ -42,3 +43,4 @@ elif inital_enemy_placement_row == 5 and inital_enemy_placement_collum == 5:
     enemy_rotation = random.randint(0,1)
     if enemy_rotation == 1:
         enemy_rotation = 3
+print(inital_enemy_placement_row,inital_enemy_placement_collum,enemy_rotation)
