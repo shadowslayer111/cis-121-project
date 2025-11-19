a729f3c8474d1c2bd83bb19a61396c146dc47f66
Jack McCarthy <26972048+shadowslayer111@users.noreply.github.com>
2025-11-19 13:51:02 -0600

    Updated make enemy
    Now instead of returning just a rotation value a list of the coordinates are returned in a list

--- Game.py
+++ Game.py
@@ -16,31 +16,50 @@ game_board = create_board(5) #hardcodes the gameboard size, maybe make it dynami
 #user_collum_guess = int(user_guess[-1]) #Take the second part of the user guess
 #game_board[user_row_guess, user_collum_guess] = "x"
 #Very jankey way to do exention with 0 being up,1 being right,2 being down and 3 being left
-inital_enemy_placement_row = random.randint(1,5)
-inital_enemy_placement_collum = random.randint(1,5)
-enemy_rotation = random.randint(0,3)
+def rotation_num_to_position(inital_pos,rotation_num):
+    if rotation_num == 0:
+        extention_position = int(inital_pos[0][0]) + 1
+        return f"{extention_position},{inital_pos[0][2]}"
+    if rotation_num == 1:
+        extention_position = int(inital_pos[0][2]) + 1
+        return f"{inital_pos[0][0]},{extention_position}"
+    if rotation_num == 2:
+        extention_position = int(inital_pos[0][0]) - 1
+        return f"{extention_position},{inital_pos[0][2]}"
+    if rotation_num == 3:
+        extention_position = int(inital_pos[0][2]) - 1
+        return f"{inital_pos[0][0]},{extention_position}"
+
+#Makes an enemy with and outputs a list with the cordinates of the two positions the ship is in    
+def make_enemy():
+    inital_enemy_placement_row = random.randint(1,5)
+    inital_enemy_placement_collum = random.randint(1,5)
+    enemy_rotation = random.randint(0,3)
 #For if inital placement is on the edge to prevent going past the playable area
-if inital_enemy_placement_row == 1:
-    enemy_rotation = random.randint(1,3)
-elif inital_enemy_placement_row == 5:
-    enemy_rotation = random.randint(0,2)
-    if enemy_rotation == 2:
-        enemy_rotation = 3
-if inital_enemy_placement_collum == 1:
-    enemy_rotation = random.randint(0,2)
-elif inital_enemy_placement_collum == 5:
-    enemy_rotation = random.randint(0,2)
-    if enemy_rotation == 1:
-        enemy_rotation = 3
+    if inital_enemy_placement_row == 1:
+        enemy_rotation = random.randint(1,3)
+    elif inital_enemy_placement_row == 5:
+        enemy_rotation = random.randint(0,2)
+        if enemy_rotation == 2:
+            enemy_rotation = 3
+    if inital_enemy_placement_collum == 1:
+        enemy_rotation = random.randint(0,2)
+    elif inital_enemy_placement_collum == 5:
+        enemy_rotation = random.randint(0,2)
+        if enemy_rotation == 1:
+            enemy_rotation = 3
 #For edge cases if the selection is in the corner
-if inital_enemy_placement_row == 1 and inital_enemy_placement_collum == 1:
-    enemy_rotation = random.randint(2,3)
-elif inital_enemy_placement_row == 1 and inital_enemy_placement_collum == 5:
-    enemy_rotation = random.randint(1,2)
-elif inital_enemy_placement_row == 5 and inital_enemy_placement_collum == 1:
-    enemy_rotation = random.randint(0,1)
-elif inital_enemy_placement_row == 5 and inital_enemy_placement_collum == 5:
-    enemy_rotation = random.randint(0,1)
-    if enemy_rotation == 1:
-        enemy_rotation = 3
-print(inital_enemy_placement_row,inital_enemy_placement_collum,enemy_rotation)
+    if inital_enemy_placement_row == 1 and inital_enemy_placement_collum == 1:
+        enemy_rotation = random.randint(2,3)
+    elif inital_enemy_placement_row == 1 and inital_enemy_placement_collum == 5:
+        enemy_rotation = random.randint(1,2)
+    elif inital_enemy_placement_row == 5 and inital_enemy_placement_collum == 1:
+        enemy_rotation = random.randint(0,1)
+    elif inital_enemy_placement_row == 5 and inital_enemy_placement_collum == 5:
+        enemy_rotation = random.randint(0,1)
+        if enemy_rotation == 1:
+            enemy_rotation = 3
+    enemy_placement = [f"{inital_enemy_placement_row},{inital_enemy_placement_collum}"]
+    enemy_placement.append(str(rotation_num_to_position(enemy_placement,enemy_rotation)))
+    return enemy_placement
+print(make_enemy())
